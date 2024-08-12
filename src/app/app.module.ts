68420cd4e17abb8d7e5aab4f6c1021abc8757bcb
David De Los Santos <ddelossantos@neural.com.do>
2024-08-12 15:18:10 -0400

    Updating app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -9,22 +9,36 @@ import { CountriesComponent } from './components/countries/countries.component';
 import { ButtonModule } from 'primeng/button'
 import { HttpClientModule } from '@angular/common/http';
 import { CardModule } from 'primeng/card';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputIconModule } from 'primeng/inputicon';
+import { DropdownModule } from 'primeng/dropdown';
+import { CountriesListComponent } from './components/countries/countries-list/countries-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountriesComponent,
     LayoutComponent,
+    CountriesListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     NgbModule,
     ButtonModule,
     HttpClientModule,
-    CardModule
+    CardModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule ,
+    DropdownModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
